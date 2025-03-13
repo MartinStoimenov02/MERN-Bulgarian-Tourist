3f27b01426b8e6ccfd4a8910bd3fa05f1735babf
MartinStoimenov02 <mgs_martin_georgiev_02@abv.bg>
2025-03-13 23:56:27 +0200

    add nto model

---
+++ server/models/nationalSites.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const NationalSiteScheme = new mongoose.Schema({
+    name: {
+        type: String,
+        required:[true, "name is required."]
+    },
+    description: {
+        type: String,
+        required:[true, "description is required."]
+    },
+    imgPath: {
+        type: String,
+        required:[true, "mapUrl number is required."]
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    numberInNationalList: {
+        type: String,
+        required:[true, "numberInNationalList is required."]
+    },
+    location: {
+        type: Object
+    },
+    google_external_id: {
+        type: String
+    },
+}, {timestamps:true});
+
+const NationalSiteModel = mongoose.model("national-site", NationalSiteScheme);
+
+export default NationalSiteModel;
